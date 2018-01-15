a231db0183f596af2952d14128ca6a5d18b3ee3e
Dean <814731008@qq.com>
2018-01-15 17:10:25 +0800

    task30

---
+++ task30/php/accounts.php
@@ -0,0 +1,39 @@
+<?php
+    //设置页面内容是html编码格式是utf-8
+    // header("Content-Type: text/plain;charset=utf-8");
+    header("Content-Type: application/json;charset=utf-8");
+    header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Methods:POST,GET");
+    //header("Content-Type: text/xml;charset=utf-8");
+    //header("Content-Type: text/html;charset=utf-8");
+    //header("Content-Type: application/javascript;charset=utf-8");
+    $accounts = array(
+        '18718401785' => 'wcz41592653',
+        '137831244@qq.com' => 'wcz41592653',
+        '814731008@qq.com' => '12456'
+    );
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        search();
+    }
+
+    function search(){
+        if (!isset($_POST['username']) || empty($_POST['username'])) {
+            echo '{"success":false,"msg":"请填写用户名"}';
+            return;
+        }
+
+        global $accounts;
+        $username = $_POST['username'];
+        $result = '{"success":false,"msg":"帐号不存在"}';
+        foreach ($accounts as $key => $value) {
+            if ($key == $username) {
+                $result = '{"success":true,"msg":"帐号填写正确"}';
+                break;
+            }
+        }
+        echo $result;
+    }
+
+
+ ?>
